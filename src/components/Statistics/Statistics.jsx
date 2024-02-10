0e289c66f4f6f3e6355d7f1a2533da1626c8a323
Vladyslav Lytvyniuk <lytvyniuk.vladyslav@gmail.com>
2024-02-10 18:55:02 +0100

    task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,24 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
+const icons = {
+  1: FaRegThumbsUp,
+  2: MdPeople,
+  3: MdOutlineProductionQuantityLimits,
+  4: GiTreeDoor,
+};
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h2 className={style.title}>{title}</h2>}
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+      <ul className={style.list}>
+        {stats.map(stat => (
+          <StatisticsItem stats={stat} key={stat.id} icon={icons[stat.id]} />
+        ))}
+      </ul>
+    </>
+  );
 };
