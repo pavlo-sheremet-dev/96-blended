81f56e0e9bf943856f1d037eeee0b90b8426f819
Dmytro Tkach <dreamworkshow@gmail.com>
2024-02-10 19:37:57 +0100

    task3

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import {
 
 import article from './data/article.json';
 import stats from './data/stats.json';
+import forbes from './data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
@@ -29,7 +31,7 @@ export const App = () => {
         <Statistics stats={stats} title="Main Statistics" />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,26 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import css from './ForbesList.module.css';
+import { ForbesListItem } from '../ForbesListItem/ForbesListItem';
+export const ForbesList = ({ list }) => {
+  return (
+    <div className={css.board}>
+      <div className={css.header}>
+        <h2 className={css.title}>
+          <span className={css.titleTop}>Forbes</span>
+          <span className={css.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={css.list}>
+        {list.map(item => (
+          <ForbesListItem
+            key={item.id}
+            avatar={item.avatar}
+            capital={item.capital}
+            isIncrease={item.isIncrease}
+            name={item.name}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
