770c372b71507395be04a02e6dd860859571e1f4
katerynazhvanko <kateryna.tertychna@gmail.com>
2024-02-10 19:20:56 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import {
   ForbesList,
 } from './components';
 
+import article from './data/article.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          avatar={article.avatar}
+          description={article.description}
+          name={article.name}
+          postedAt={article.postedAt}
+          poster={article.poster}
+          tag={article.tag}
+          title={article.title}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
