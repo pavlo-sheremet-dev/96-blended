8ff1fabbfa285f50ef3dfcde34b1b5a93e869ad5
Vladyslav Lytvyniuk <lytvyniuk.vladyslav@gmail.com>
2024-02-10 20:01:01 +0100

    task-4

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 import article from './data/article.json';
 import stats from './data/stats.json';
 import forbes from './data/forbes.json';
-
+import transactions from './data/transactions.json';
 export const App = () => {
   return (
     <Section>
@@ -34,7 +34,7 @@ export const App = () => {
         <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
